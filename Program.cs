722f16c3a125f1763c6b25dc44f8cebe34659dd8
Teflon Dervish <voltvilty45@gmail.com>
2023-12-08 11:24:43 +0500

    Make func

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿string[] FilterStrings(string[] arr){
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++){
+        if (arr[i].Length <= 3){
+            count++;
+        }
+    }
+
+    string[] result = new string[count];
+    int index = 0;
+    for (int i = 0; i < arr.Length; i++){
+        if (arr[i].Length <= 3){
+            result[index] = arr[i];
+            index++;
+        }
+    }
+
+    return result;
+}
